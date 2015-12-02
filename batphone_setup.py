68afb875ea257c0c486e9014ea8e0561a375b217
squidwardT <lavigne.de@husky.neu.edu>
2015-12-02 14:48:15 -0500

    IP management

--- batphone_setup.py
+++ batphone_setup.py
@@ -36,10 +36,13 @@ def setup():
 	import sys
 	sys.path.append('..')
 	import application_management
+	import threading
+	from sockets import TCPServerSocket
 	from join_wifi import join_wifi
 	from join_batman_network import join_batman_network
 	from post_network_device import post_network_device
 	
+	# Pull info from form
 	dev_name = request.form['name']
 	longitude = request.form['longitude']
 	latitude = request.form['latitude']
@@ -47,13 +50,27 @@ def setup():
 	notes = request.form['notes']
 	ssid = request.form['ssid']
 	mac = request.form['mac']
-		
+	
+	# Join the BATMAN network in the DHCP/Web App Host spot
 	join_batman_network(interface = 'wlan0', network_name = ssid, ap_mac = mac)
+
+	# Creates a thread to manage new network devices and 
+        # dish out open IP addresses.           
+        socket = TCPSocketServer.TCPSocketServer('192.168.2.4')
+        dhcp_thread = threading.Thread(target = socket.start_server)
+        dhcp_thread.start()
+	
+	# Get the data from the server
 	data = application_management.database_clone.database_clone()
+	
+	# Add the device's information to the database
 	post_network_device(ssid = ssid, publickey = 'pgp', dev_name = dev_name,
-			    IP_address = '0.0.0.0', MAC = mac, Gateway_mode = 'client', 
+			    IP_address = '192.168.2.4', MAC = mac, Gateway_mode = 'client', 
 			    description = description, longitude = longitude, latitude = latitude,
 			    notes = notes)
+	
+	# Start hosting the web app, add the info to the database
+	# Start managing the database syncing
 	application_management.host_web_app.host_web_app()
 	application_management.populate_db.populate_db(data)
 	application_management.manage_db.manage_db()
--- sockets/TCPServerSocket.py
+++ sockets/TCPServerSocket.py
@@ -16,13 +16,13 @@ class TCPServerSocket(TCPSocket):
                                    device in some way.
     '''
 
-    def __init__(self, address):
+    def __init__(self, address, port):
         '''CONSTRUCTOR: Run parent constructor and initialize the clients array
         '''
         super(TCPServerSocket, self).__init__(address)
         self.clients = []
         self.address = address
-        self.port = 55704
+       	self.port = port
 
     def start_server(self, interpreter = None):
         '''Listen on a @port for incoming application connections. When connected to
@@ -36,7 +36,7 @@ class TCPServerSocket(TCPSocket):
         '''
 
         # Bind this socket to port and listen for connections
-        self.sock.bind(('0.0.0.0', self.port))
+        self.sock.bind((self.address, self.port))
         self.sock.listen(5)
 
         # While the application is running attempt listen for connections and read the
@@ -52,53 +52,7 @@ class TCPServerSocket(TCPSocket):
                 target=self.read_client, args=[tcp_client, interpreter])
             read_thread.start()
 
-    def update_client(self):
-        '''Update the list of known clients removing all clients that are not readable
-           or writable or are in error.
-
-        ARGS:
-        None
-
-        RETURNS:
-        None
-        '''
-        # Check the states of clients
-        ready_to_read, ready_to_write, in_error = select.select(self.clients,
-                                                                self.clients,
-                                                                self.clients,
-                                                                60.0)
-        # Remove items in error
-        for item in in_error:
-            self.clients.remove(item)
-        # Remove items not readable or writable
-        for item in self.clients:
-            if item not in ready_to_read and item not in ready_to_write:
-                self.clients.remove(item)
-        # Read ready to read clients
-        for item in ready_to_read:
-            self.read_client(item)
-
-    def add_client(self, clients):
-        '''Add a client to the list of recent client. If it already exists update it.
-
-        ARGS:
-        @clients		-- The client to be added to the list
-
-        RETURNS:
-        None
-        '''
-        # Update client list and read out inbound messages
-        client = clients
-        self.update_client(client)
-
-        # If the newly connected device is already in list update it otherwise
-        # add it
-        for item in self.clients:
-            if item.address == client.address:
-                self.clients.remove(item)
-        self.clients.append(client)
-
-    def read_client(self, clients, interpreter):
+    def read_client(self, clients):
         '''Receive the client's message.
 
         ARGS:
@@ -108,21 +62,16 @@ class TCPServerSocket(TCPSocket):
         RETURNS:
         @args 			-- A list of arguments to be interpreted
         '''
-        msg = interpreter.interpret(clients.read())
+	import shlex
+        msg = clients.read()
+	args = shlex.split(msg)
 
-        if msg is not None:
-            print msg
-            respond_to_client(clients, msg)
-
-    def respond_to_client(self, client, msg):
-        from TCPSocket import TCPSocket
-        port = find_tcp_port(client.address)
-
-        responder = TCPSocket(self.address)
-        responder.connect(client.address, port)
-        responder.write(msg)
-        responder.close()
-
+        if args[0] == 'DHCP':
+		# NEED TO WRITE
+		ip = find_open_ip(args[1])
+		clients.sendall(ip)
+	else:
+		print 'Invalid Request ' + msg
 
 if __name__ == '__main__':
     server = TCPServerSocket('whatever')
